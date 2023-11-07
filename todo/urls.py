d8df4be42fbc681c98f97599ad9fdd8d523017a5
Shirjeel Afzal <shirjeelafzals@gmail.com>
2023-11-07 17:04:51 +0500

    1.2

--- todo/urls.py
+++ todo/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
+    #urls for Read
     path('task',views.todo_task),
     path('user',views.todo_user),
     path('file',views.todo_file),
     path('history',views.todo_history),
 
-    
+
     path('todo/<int:pk>',views.todo_list_pk,name='display'),
 ]
